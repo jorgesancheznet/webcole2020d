52bcbaa6f28878f1ac86277f78cb68087277e851
jorgesancheznet <info@jorgesanchez.net>
2020-12-11 19:07:18 +0100

    SplashPanel completo

---
+++ components/PanelNovedades.vue
@@ -0,0 +1,77 @@
+<template>
+  <aside id="novedades"
+         data-aos="fade-left" :data-aos-delay="tiempoAparicion" :data-aos-duration="tiempoTransicion" v-if="mostrarPanel">
+    <h2>{{titulo}}</h2>
+    <ul>
+      <li v-for="entrada in entradas"><a :href="entrada.urlMas">{{entrada.titulo}}</a></li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: "PanelNovedades",
+  props:{
+    titulo:String,
+    entradas:Array,
+    tiempoTransicion:{
+      type:String,
+      default:"5000"
+    },
+    tiempoAparicion:{
+      type:String,
+      default:"2000"
+    }
+  },
+  data(){
+    return{
+      mostrarPanel:false
+    }
+  },
+  mounted(){
+    setInterval(()=>{this.mostrarPanel=true},2000);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/estilos/mixins.scss";
+@import "~assets/estilos/transiciones.scss";
+
+
+#novedades{
+  @include letraNormal(light,1.2em,$colorTenue);
+  position:absolute;
+  top:$altoCabecera + 40px;
+  right:2em;
+  padding:0;
+  color:$colorTenue;
+  border:1px solid $colorFicha;
+  font-size:1.5em;
+  h2{
+    @include letraNormal(light,1.2em,$colorGrisSalesiano);
+    text-align: center;
+    margin:0;
+    background-color: $colorFicha;
+  }
+  ul{
+    margin:0;
+    padding:0 1em 0 0;
+    list-style-type: none;
+    li{
+      margin:.1em 0 .5em 1em;
+      a{
+        color:$colorTenue;
+        &:hover{
+          color:$colorTenueBrillo;
+        }
+      }
+    }
+  }
+}
+@media screen and  (max-width: $anchoMovil){
+  #novedades{
+    font-size:1em;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <SplashPanel idSeccion="Portada" imagen="/img/neonbrand-426918-unsplash.jpg" titulo="Excelencia adaptada<br>a cada estudiante" subtitulo="En búsqueda permanente de nuevos caminos que permitan el éxito educativo">
+      <PanelNovedades titulo="NOVEDADES" :entradas="listaNovedades" tiempoTransicion="1000"  tiempoAparicion="300" />
 
     </SplashPanel>
   </main>
@@ -8,15 +9,22 @@
 
 <script>
 import Cabecera from "~/components/cabecera/Cabecera";
-import SplashPanel from "@/components/secciones/SplashPanel";
+import SplashPanel from "~/components/secciones/SplashPanel";
+import PanelNovedades from "~/components/PanelNovedades";
 
+import {listaNovedades} from "~/assets/data/novedades";
 import {mapMutations} from "vuex";
 
 export default {
-  components: {SplashPanel, Cabecera},
+  components: {PanelNovedades, SplashPanel, Cabecera},
   computed:{
     fondo(){
       return this.$store.state.fondo
+    }
+  },
+  data(){
+    return{
+      listaNovedades:listaNovedades.slice(0,3)
     }
   },
   methods:{
