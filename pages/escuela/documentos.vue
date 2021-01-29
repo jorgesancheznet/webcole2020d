ad3c8b679a7f404d8753c071c7bd3c5ccfa3bfb4
jorgesancheznet <info@jorgesanchez.net>
2021-01-29 13:56:02 +0100

    Erasmus hecho

---
+++ pages/escuela/documentos.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <TituloGrande titulo="Documentos"
+                  imagen="/img/escuela/documentos.jpg"/>
+    <SeccionOpaca>
+
+    </SeccionOpaca>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "erasmus"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/escuela/erasmus.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <TituloGrande titulo="Servicio Erasmus+" subtitulo="Fomento de la educación en Europa"
+                  imagen="/img/escuela/europa.jpg"/>
+    <SeccionOpaca>
+      <FilaContenido>
+        <ColumnaContenido>
+          <TextoDetalle titulo="¿Qué es Erasmus+?">
+            <p>El <a href="http://sepie.es/" target="_blank">Servicio Español para la Internacionalización de la Educación</a> nos concede para este curso el programa
+              <a href="https://ec.europa.eu/programmes/erasmus-plus/node_es/" target="_blank">ErasmusPlus</a>, el programa de la Unión Europea para apoyar la educación, la formación, la juventud y el
+              deporte en Europa.</p>
+          </TextoDetalle>
+        </ColumnaContenido>
+        <ColumnaContenido>
+          <ImagenDetalle alt="Erasmus plus" src="/img/escuela/Erasmus_SEPIe.jpeg"/>
+        </ColumnaContenido>
+      </FilaContenido>
+    </SeccionOpaca>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "erasmus"
+}
+</script>
+
+<style scoped>
+
+</style>
