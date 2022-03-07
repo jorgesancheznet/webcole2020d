533334f8e584f42da1b1eb77262a4306d8184c25
jorgesancheznet <info@jorgesanchez.net>
2022-03-07 22:45:05 +0100

    Puertas abiertas ESO, vídeos incrustados

---
+++ components/wrappers/ContenedorVideos.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <article class="contenedorVideos" >
+      <iframe width="560" height="315" :src="video.ruta" :title="video.title" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen v-for="video in videos"></iframe>
+    </article>
+  </div>
+</template>
+
+<script>
+
+
+import {noticias} from "assets/data/noticias";
+
+export default {
+  name: "ContenedorVideos",
+  props: {
+    videos: Array
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/estilos/mixins.scss";
+
+.contenedorVideos {
+  text-align: center;
+  width: 100%;
+}
+
+iframe {
+  display: inline-block;
+  width: 47%;
+  margin: .5em;
+  cursor: pointer;
+
+  &:hover {
+    outline: solid 4px white;
+  }
+
+  img {
+    width: 100%;
+  }
+}
+
+
+@media (max-width: $anchoMedio) {
+  iframe {
+    width: 95%
+  }
+}
+
+</style>
--- pages/noticias/2022/03/puertas-abiertas.vue
+++ pages/noticias/2022/03/puertas-abiertas.vue
@@ -1,13 +1,32 @@
 <template>
-  <PaginaNoticia titulo="La evaluación como instrumento de aprendizaje"
+  <PaginaNoticia titulo="Jornada de Puertas Abiertas, ESO"
                  imagen="/img/textura1.jpg">
     <FilaContenido>
       <TextoDetalle>
-        <p><strong>Jornada de Puertas Abiertas</strong> para <strong>Educación Secundaria</strong> jueves 10 de marzo a las 17:00</p>
-        <p>¡Ven a conocernos! Excelencia adaptada a cada estudiante</p>
-        <p><a href="https://drive.google.com/file/d/1z0OphGRleE19RcYtIwSIEv-3dJwdYOst/view">Ver vídeo</a></p>
+        <p>En una sociedad cada vez más compleja que avanza a pasos agigantados hacia el mundo digital y las nuevas
+          tecnologías. Nuestro centro apuesta por un aprendizaje que desarrolla habilidades y competencias
+          indispensables para la sociedad del S.XXI, con una visión creativa, social e inclusiva.</p>
+        <p>La Innovación Educativa forma parte de los grandes planes de acción que estamos desarrollando en nuestro
+          centro, coherentes con los valores de nuestra Propuesta Educativa.</p>
+        <p>Poseemos un amplio recorrido en la integración de nuevas metodologías y en la incorporación de las TICs
+          (reconocimiento Codice TIC con nivel máximo) a nuestro proceso de enseñanza-aprendizaje. Además, este
+          curso la Junta de Castilla y León nos ha seleccionado como único centro de secundaria en la provincia de
+          Palencia para formar parte del proyecto de innovación educativa: <strong>Espacios Flexibles de Formación y
+          aprendizaje</strong> que promueve la transformación de la enseñanza para que el alumno sea protagonista de su propio
+          aprendizaje.</p>
+        <p>Apostamos por el aprendizaje individualizado y adaptado a las necesidades de cada alumno, lo que permite alcanzar los máximos niveles
+          de aprendizaje.</p>
+        <p>Somos un centro con proyección profesional. Nuestra oferta educativa ofrece Ciclos formativos de grado medio y
+          superior y FPB. Contamos con programas <NuxtLink to="/escuela/erasmus">Erasmus+</NuxtLink> y
+          <NuxtLink to="/escuela/fp-dual">FP Dual</NuxtLink> y poseemos reconocimiento como
+          <NuxtLink to="/escuela/excelencia">Centro de excelencia
+            profesional de alto nivel
+          </NuxtLink>.</p>
+        <p>Si quieres saber más puedes descargar nuestro
+          <NuxtLink to="/docs/Folleto-ESO.pdf" target="_blank" >folleto informativo</NuxtLink></p>
       </TextoDetalle>
     </FilaContenido>
+    <ContenedorVideos :videos="videos"></ContenedorVideos>
 
 
   </PaginaNoticia>
@@ -19,9 +38,33 @@ import PaginaNoticia from "@/components/secciones/PaginaNoticia";
 import FilaContenido from "@/components/wrappers/FilaContenido";
 import ColumnaContenido from "@/components/wrappers/ColumnaContenido";
 import TextoDetalle from "@/components/wrappers/TextoDetalle";
+import ContenedorVideos from "@/components/wrappers/ContenedorVideos";
+import {noticias} from "assets/data/noticias";
 
 export default {
-  components: {PaginaNoticia, FilaContenido, ColumnaContenido, TextoDetalle}
+  components: {PaginaNoticia, FilaContenido, ColumnaContenido, TextoDetalle,ContenedorVideos},
+  data() {
+    return {
+      videos:[
+        {
+          ruta:"https://www.youtube.com/embed/cHdEr7stxIQ",
+          title:"Innovación y digitalización"
+        },
+        {
+          ruta:"https://www.youtube.com/embed/g0BcBoIaFAs",
+          title:"Mediación"
+        },
+        {
+          ruta:"https://www.youtube.com/embed/PuBQIsGe180",
+          title:"Educando mente y corazón"
+        },
+        {
+          ruta:"https://www.youtube.com/embed/cCW1TKsfSjc",
+          title:"Testimonios"
+        }
+      ]
+    }
+  }
 }
 </script>
 
